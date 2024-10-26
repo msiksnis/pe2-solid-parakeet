8a3f8ef95160a92fa7c4baee8031eb17a8ccb2a0
Martins Siksnis <msiksnis@gmail.com>
2024-10-26 20:22:32 +0200

    fixed logo path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 export default function Navbar() {
   return (
     <div className="mx-auto flex w-full max-w-6xl items-center px-10 py-4 xl:px-0">
-      <img src="/public/Logo.png" className="max-h-14" />
+      <img src="/Logo.png" className="max-h-14" />
       <div className="ml-auto flex items-center space-x-32">
         <div className="space-x-10">
           <Link
