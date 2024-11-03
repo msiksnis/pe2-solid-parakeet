83a35deabcb7fb35e405aed4ddf6fb91805722e0
Martins Siksnis <msiksnis@gmail.com>
2024-11-03 10:11:07 +0100

    commit before new branch

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,11 @@
-import footer_img from "../../public/mountain-cropped.webp";
-
 export default function Footer() {
   return (
     <div className="">
       <div
         className="relative -z-10 h-[600px] w-full bg-cover bg-bottom"
-        style={{ backgroundImage: `url(${footer_img})` }}
+        style={{
+          backgroundImage: `url(${"../../mountain-cropped.webp"})`,
+        }}
       >
         <div className="absolute bottom-0 left-0 h-[600px] w-full bg-gradient-to-b from-white to-white/80" />
       </div>
